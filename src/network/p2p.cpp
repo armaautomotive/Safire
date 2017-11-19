c545e8e6a1375d6895493ce77a5b90a1fc57ba79
jon34560 <jondtaylor@gmail.com>
2017-11-18 20:56:47 -0800

    Work on p2p code.

--- src/network/p2p.cpp
+++ src/network/p2p.cpp
@@ -17,5 +17,60 @@ CP2P::CP2P(){
 
     g_networking_init(); 
 
+    gloop = g_main_loop_new(NULL, FALSE);
+
+    
+    #ifdef G_OS_WIN32
+    io_stdin = g_io_channel_win32_new_fd(_fileno(stdin));
+    #else
+    io_stdin = g_io_channel_unix_new(fileno(stdin));
+    #endif
+
+    
+    // Create the nice agent
+    agent = nice_agent_new(g_main_loop_get_context (gloop), NICE_COMPATIBILITY_RFC5245);
+    if (agent == NULL)
+        g_error("Failed to create agent");
+    
+    // Set the STUN settings and controlling mode
+    if (stun_addr) {
+         g_object_set(agent, "stun-server", stun_addr, NULL);
+         g_object_set(agent, "stun-server-port", stun_port, NULL);
+    }
+    g_object_set(agent, "controlling-mode", controlling, NULL);
+
+    // Connect to the signals
+    //g_signal_connect(agent, "candidate-gathering-done", G_CALLBACK(cb_candidate_gathering_done), NULL);
+    //g_signal_connect(agent, "new-selected-pair", G_CALLBACK(cb_new_selected_pair), NULL);
+    //g_signal_connect(agent, "component-state-changed", G_CALLBACK(cb_component_state_changed), NULL);
+
+
 
 }
+
+
+void CP2P::connect(){
+
+	
+
+}
+
+
+static void cb_candidate_gathering_done(NiceAgent *agent, guint _stream_id, gpointer data)
+{
+
+  g_debug("SIGNAL candidate gathering done\n");
+
+  // Candidate gathering is done. Send our local candidates on stdout
+  printf("Copy this line to remote client:\n");
+  printf("\n  ");
+  print_local_data(agent, _stream_id, 1);
+  printf("\n");
+
+  // Listen on stdin for the remote candidate list
+  printf("Enter remote data (single line, no wrapping):\n");
+  g_io_add_watch(io_stdin, G_IO_IN, stdin_remote_info_cb, agent);
+  printf("> ");
+  fflush (stdout);
+}
+
--- src/network/p2p.h
+++ src/network/p2p.h
@@ -28,23 +28,34 @@
 //#include <string.h>
 //#include <assert.h>
 
-#define CHUNK_SIZE 1024
+//define CHUNK_SIZE 1024
 static const gchar *candidate_type_name[] = {"host", "srflx", "prflx", "relay"};
 static const gchar *state_name[] = {"disconnected", "gathering", "connecting", "connected", "ready", "failed"};
 
+static int print_local_data(NiceAgent *agent, guint stream_id, guint component_id);
+static int parse_remote_data(NiceAgent *agent, guint stream_id, guint component_id, char *line);
+static void cb_candidate_gathering_done(NiceAgent *agent, guint stream_id, gpointer data);
+static void cb_new_selected_pair(NiceAgent *agent, guint stream_id, guint component_id, gchar *lfoundation, gchar *rfoundation, gpointer data);
+static void cb_component_state_changed(NiceAgent *agent, guint stream_id, guint component_id, guint state, gpointer data);
+static void cb_nice_recv(NiceAgent *agent, guint stream_id, guint component_id, guint len, gchar *buf, gpointer data);
+static gboolean stdin_remote_info_cb (GIOChannel *source, GIOCondition cond, gpointer data);
+static gboolean stdin_send_data_cb (GIOChannel *source, GIOCondition cond, gpointer data);
+
+static GMainLoop *gloop;
+static GIOChannel* io_stdin;
 
 class CP2P
 {
 private:
     NiceAgent *agent;
 
-    static GMainLoop *gloop;
-    static GIOChannel* io_stdin;
+    //static GMainLoop *gloop;
+    //static GIOChannel* io_stdin;
     static guint stream_id;
 
     gchar *stun_addr = NULL;
     guint stun_port = 0; 
-    gboolean controlling ;
+    gboolean controlling;
 
 
 public:
@@ -55,10 +66,8 @@ public:
     {
     }
 
-    
-    //bool fileExists(std::string fileName);
-    //bool write(std::string privateKey, std::string publicKey);
-    //bool read(std::string & privateKey, std::string & publicKey);
+    void connect();    
+    //void cb_candidate_gathering_done(NiceAgent *agent, guint stream_id, gpointer data);
 };
 
 #endif // P2P_H
