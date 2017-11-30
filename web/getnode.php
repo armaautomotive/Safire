3e15f36c14424efcef95badbdd898d14b91d9526
jon34560 <jondtaylor@gmail.com>
2017-11-30 15:35:27 -0800

    Update network peer endpoint. still needs work, it doesn't correctly pair nodes.

--- web/getnode.php
+++ web/getnode.php
@@ -5,13 +5,19 @@
 * Description: This is a temporary server endpoint that will connect nodes to each other for the first time.
 *       After a set of nodes is shared among nodes they can relay connection data for additional nodes.
 *
-* @param 'r' p2p connection string
+* TODO: This endpoint needs to keep track of connection pairs to that both nodes receive the others connection string.
+* 
+* @param 'connection_string' p2p connection string
+* @param 'public_key' user public key to identify 
 */
 header('Content-Type: application/json');
 
 include_once("db.php");
 
 date_default_timezone_set('America/Los_Angeles');
+
+$public_key = isset( $_POST['public_key'] ) ? $_POST['public_key'] : $_GET['public_key'];
+$connection_string = isset( $_POST['connection_string'] ) ? $_POST['connection_string'] : $_GET['connection_string'];
 
 $clear = $_GET['clear'];
 if($clear == "true"){
@@ -27,41 +33,51 @@ if($clear == "true"){
         exit;
 }
 
-$requester = $_POST['r'] || $_GET['r'];
-if($requester == ''){
+if($connection_string == ''){
         $response = array();
-        $response["error"] = "Error: no 'r' parameter provided.";
+        $response["error"] = "Error: no 'connection_string' parameter provided.";
         echo json_encode($response);
         exit;
 }
-
+if($public_key == ''){
+        $response = array();
+        $response["error"] = "Error: no 'public_key' parameter provided.";
+        echo json_encode($response);
+        exit;
+}
 $pdo = new PDO('mysql:dbname='.$db_name.';host='.$db_host, $db_user, $db_pass);
 $params = array();
-$params["connection_string"] = $_POST['r'] || $_GET['r'];
-$sql = "select * from node_list where connection_string = :connection_string order by time desc limit 0, 1;";
+$params["public_key"] = $public_key;
+$sql = "select * from node_list where public_key = :public_key order by time desc limit 0, 1;";
 $stmt = $pdo->prepare($sql);
 $r = $stmt->execute($params);
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 if( sizeof($results) == 0 ){
         // Add current user to the database.
-        $insert_sql = "insert into node_list (connection_string) values (:connection_string);";
+        $params = array();
+        $params["public_key"] = $public_key;
+        $params["connection_string"] = $connection_string;
+        $insert_sql = "insert into node_list (public_key, connection_string) values (:public_key, :connection_string);";
         $stmt = $pdo->prepare($insert_sql);
         $stmt->execute($params);
 
-	// Purge old data
+        // Purge old data
         $delete_sql = "delete from node_list where time < UNIX_TIMESTAMP(DATE_SUB(NOW(), INTERVAL 30 DAY))";
         $stmt = $pdo->prepare($delete_sql);
         $result = $stmt->execute($params);
 }
 
-$sql = "select * from node_list where connection_string != :connection_string order by time desc limit 0, 100;";
+$sql = "select * from node_list where public_key != :public_key order by time desc limit 0, 100;";
 $stmt = $pdo->prepare($sql);
+$params = array();
+$params["public_key"] = $public_key;
 $r = $stmt->execute($params);
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 if( sizeof($results) > 0 ){
-	$data = array();
+        $data = array();
         foreach($results as $result){
                 $data_record = array();
+                $data_record["public_key"] = $result["public_key"];
                 $data_record["connection_string"] = $result["connection_string"];
                 array_push($data, $data_record);
         }
@@ -75,4 +91,6 @@ if( sizeof($results) > 0 ){
         exit;
 }
 ?>
+
+
 
